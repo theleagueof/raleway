4db9f5a40757c04ed486c1c682d9bf6864e372bf
CJ Dunn <cjdunn@Lappy-447.local>
2019-10-18 14:11:39 -0400

    try building TTFs from exported .designspace files

---
+++ source/build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -e
+
+
+# echo "Generating Static fonts"
+# mkdir -p ../fonts
+# fontmake -m Raleway-Roman.designspace -i -o ttf --output-dir ../fonts/ttf/
+# fontmake -m Raleway-Italic.designspace -i -o ttf --output-dir ../fonts/ttf/
+
+
+
+
+
+# echo "Generating VFs"
+# fontmake -m Rubik.designspace -o variable --output-path ../fonts/ttf/Rubik[wght].ttf
+# fontmake -m Rubik_Italic.designspace -o variable --output-path ../fonts/ttf/Rubik-Italic[wght].ttf
+
+# rm -rf master_ufo/ instance_ufo/ instance_ufos/*
+
+
+rm -rf master_ufo/ instance_ufo/ instance_ufos/*
