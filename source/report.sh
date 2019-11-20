637959b8e6260c431fcb4dfdd7e2a1748fc7fb32
CJ Dunn <cjdunn@Lappy-447.local>
2019-11-20 13:04:57 -0500

    uncomment build scripts

--- source/report.sh
+++ source/report.sh
@@ -5,3 +5,9 @@ set -e
 gftools qa -f ../fonts/vf/Raleway[wght].ttf  -gfb --fontbakery --diffenator -o ../fonts/vf/out_roman
 
 gftools qa -f ../fonts/vf/Raleway-Italic[wght].ttf  -gfb --fontbakery --diffenator -o ../fonts/vf/out_italic
+
+
+
+# gftools qa -f ../fonts/vf/Raleway[wght].ttf  -gfb --fontbakery -o ../fonts/vf/out_roman
+
+# gftools qa -f ../fonts/vf/Raleway-Italic[wght].ttf  -gfb --fontbakery  -o ../fonts/vf/out_italic
