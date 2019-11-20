fbeb8777727ea97ed7aff4a5fe7d7e2797e539b7
CJ Dunn <cjdunn@Lappy-447.local>
2019-11-20 13:07:53 -0500

    update report script

--- source/report.sh
+++ source/report.sh
@@ -2,12 +2,6 @@
 set -e
 
 
+## run reports for fontbakery and diffenator for roman and italic
 gftools qa -f ../fonts/vf/Raleway[wght].ttf  -gfb --fontbakery --diffenator -o ../fonts/vf/out_roman
-
 gftools qa -f ../fonts/vf/Raleway-Italic[wght].ttf  -gfb --fontbakery --diffenator -o ../fonts/vf/out_italic
-
-
-
-# gftools qa -f ../fonts/vf/Raleway[wght].ttf  -gfb --fontbakery -o ../fonts/vf/out_roman
-
-# gftools qa -f ../fonts/vf/Raleway-Italic[wght].ttf  -gfb --fontbakery  -o ../fonts/vf/out_italic
