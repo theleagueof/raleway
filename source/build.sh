9b89849f7331f49018c8b5e563dfd734cee9c2a2
CJ Dunn <cjdunn@Lappy-447.local>
2019-11-20 09:42:11 -0500

    temp comment out build script and report

--- source/build.sh
+++ source/build.sh
@@ -2,16 +2,16 @@
 set -e
 
 
-echo "Generating Static fonts"
-mkdir -p ../fonts
-fontmake -m Raleway-Roman.designspace -i -o ttf --output-dir ../fonts/ttf/
-fontmake -m Raleway-Italic.designspace -i -o ttf --output-dir ../fonts/ttf/
-fontmake -m Raleway-Roman.designspace -i -o otf --output-dir ../fonts/otf/
-fontmake -m Raleway-Italic.designspace -i -o otf --output-dir ../fonts/otf/
+# echo "Generating Static fonts"
+# mkdir -p ../fonts
+# fontmake -m Raleway-Roman.designspace -i -o ttf --output-dir ../fonts/ttf/
+# fontmake -m Raleway-Italic.designspace -i -o ttf --output-dir ../fonts/ttf/
+# fontmake -m Raleway-Roman.designspace -i -o otf --output-dir ../fonts/otf/
+# fontmake -m Raleway-Italic.designspace -i -o otf --output-dir ../fonts/otf/
 
 echo "Generating VFs"
 mkdir -p ../fonts/vf
-fontmake -m Raleway-Roman.designspace -o variable --output-path ../fonts/vf/Raleway[wght].ttf
+# fontmake -m Raleway-Roman.designspace -o variable --output-path ../fonts/vf/Raleway[wght].ttf
 fontmake -m Raleway-Italic.designspace -o variable --output-path ../fonts/vf/Raleway-Italic[wght].ttf
 
 rm -rf master_ufo/ instance_ufo/ instance_ufos/*
@@ -40,7 +40,7 @@ echo "Post processing VFs"
 for vf in $vfs
 do
 	gftools fix-dsig -f $vf;
-	ttfautohint-vf --stem-width-mode nnn $vf "$vf.fix";
+	ttfautohint --stem-width-mode nnn $vf "$vf.fix";
 	mv "$vf.fix" $vf;
 done
 
--- source/report.sh
+++ source/report.sh
@@ -2,6 +2,6 @@
 set -e
 
 
-gftools qa -f ../fonts/vf/Raleway[wght].ttf  -gfb --fontbakery --diffenator -o ../fonts/vf/out_roman
+# gftools qa -f ../fonts/vf/Raleway[wght].ttf  -gfb --fontbakery --diffenator -o ../fonts/vf/out_roman
 
 gftools qa -f ../fonts/vf/Raleway-Italic[wght].ttf  -gfb --fontbakery --diffenator -o ../fonts/vf/out_italic
